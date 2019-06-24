a5a2c2bf346ee45bb8b506956e7b6d6a5371a15e
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-06-25 08:53:15 +1000

    [Teams] Add and remove team members

--- app/api/business/team_business.py
+++ app/api/business/team_business.py
@@ -26,8 +26,8 @@ def update_team(data):
     if stage == 'about':
         update_team_information(team_data)
 
-    if stage == 'leads':
-        update_team_leads(team_data)
+    if stage == 'leads' or stage == 'members':
+        update_team_leads_and_members(team_data)
 
     team = teams.get_team(team_data['id'])
     return team
@@ -44,7 +44,7 @@ def update_team_information(data):
     return team
 
 
-def update_team_leads(data):
+def update_team_leads_and_members(data):
     incoming_team_leads = data.get('teamLeads', [])
     incoming_team_members = data.get('teamMembers', [])
     incoming_team_lead_ids = [int(team_lead_id) for team_lead_id in incoming_team_leads]
@@ -56,8 +56,10 @@ def update_team_leads(data):
     current_team_lead_ids = [team_lead.user_id for team_lead in current_team_leads]
     current_team_member_ids = [team_member.user_id for team_member in current_team_members]
 
-    team_leads_to_add = set(incoming_team_lead_ids) - set(current_team_lead_ids)
+    team_members_to_promote = set(incoming_team_lead_ids) & set(current_team_member_ids)
     team_leads_to_demote = set(incoming_team_member_ids) & set(current_team_lead_ids)
+
+    team_leads_to_add = set(incoming_team_lead_ids) - set(current_team_lead_ids) - set(team_members_to_promote)
     team_leads_to_remove = set(current_team_lead_ids) - set(incoming_team_lead_ids) - set(team_leads_to_demote)
 
     for user_id in team_leads_to_add:
@@ -68,6 +70,18 @@ def update_team_leads(data):
         team_members.demote_team_lead(team_id=team.id, user_id=user_id)
 
     for user_id in team_leads_to_remove:
+        user = users.remove_from_team(user_id, team.id)
+
+    team_members_to_add = set(incoming_team_member_ids) - set(current_team_member_ids) - set(team_leads_to_demote)
+    team_members_to_remove = set(current_team_member_ids) - set(incoming_team_member_ids) - set(team_members_to_promote)
+
+    for user_id in team_members_to_add:
+        user = users.add_to_team(user_id, team)
+
+    for user_id in team_members_to_promote:
+        team_members.promote_to_team_lead(team_id=team.id, user_id=user_id)
+
+    for user_id in team_members_to_remove:
         user = users.remove_from_team(user_id, team.id)
 
     return get_team(team.id)
