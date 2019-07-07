1a7483134ec9d8fcad488c67ac39a0e00473c758
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-07-08 09:35:17 +1000

    [Teams] Add service function to get team leads by user id

--- app/api/services/team_members.py
+++ app/api/services/team_members.py
@@ -1,5 +1,5 @@
 from app.api.helpers import Service
-from app.models import TeamMember
+from app.models import TeamMember, db
 
 
 class TeamMembersService(Service):
@@ -7,6 +7,14 @@ class TeamMembersService(Service):
 
     def __init__(self, *args, **kwargs):
         super(TeamMembersService, self).__init__(*args, **kwargs)
+
+    def get_team_leads_by_user_id(self, user_ids):
+        return (db.session
+                  .query(TeamMember)
+                  .filter(
+                      TeamMember.user_id.in_(user_ids),
+                      TeamMember.is_team_lead.is_(True))
+                  .all())
 
     def promote_to_team_lead(self, team_id, user_id):
         team_member = self.find(team_id=team_id, user_id=user_id).one_or_none()
