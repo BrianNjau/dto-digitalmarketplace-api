db0bb91c2e125101f51bbec2275f17a6b6e2691f
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2019-07-08 11:05:03 +1000

    [Teams] Fix full stop separating parameters to add user function

--- app/api/business/team_business.py
+++ app/api/business/team_business.py
@@ -145,7 +145,7 @@ def update_team_leads_and_members(data):
     team_leads_to_remove = set(current_team_lead_ids) - set(incoming_team_lead_ids) - set(team_leads_to_demote)
 
     for user_id in team_leads_to_add:
-        add_user_to_team(user_id. team.id)
+        add_user_to_team(user_id, team.id)
         team_members.promote_to_team_lead(team_id=team.id, user_id=user.id)
 
     for user_id in team_leads_to_demote:
@@ -158,7 +158,7 @@ def update_team_leads_and_members(data):
     team_members_to_remove = set(current_team_member_ids) - set(incoming_team_member_ids) - set(team_members_to_promote)
 
     for user_id in team_members_to_add:
-        add_user_to_team(user_id. team.id)
+        add_user_to_team(user_id, team.id)
 
     for user_id in team_members_to_promote:
         team_members.promote_to_team_lead(team_id=team.id, user_id=user_id)
