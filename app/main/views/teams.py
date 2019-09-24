d83bf08eb7e832c4d7cec56a7fd75c7deba072c6
Reshma Abraham <reshmaabraham@DTAs-MacBook-Pro.local>
2019-09-24 14:33:18 +1000

    working Team_id added teams in _init_.py

--- app/main/views/teams.py
+++ app/main/views/teams.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import jsonify, abort, current_app, request
 from sqlalchemy import desc
 from sqlalchemy.dialects.postgresql import aggregate_order_by
@@ -5,12 +6,27 @@ from app.api.helpers import Service
 from app.models import Team, TeamMember, TeamMemberPermission, User, db
 
 from app.api.business import (team_business)
+=======
+from dmutils.data_tools import ValidationError
+from dmutils.filters import timesince
+from flask import jsonify, abort, current_app, request
+from sqlalchemy import desc
+from sqlalchemy.orm import joinedload, lazyload, noload
+from sqlalchemy.exc import IntegrityError
+import pendulum
+from pendulum.parsing.exceptions import ParserError
+
+from app.api.services import (
+    team_service
+)
+>>>>>>> working Team_id added teams in _init_.py
 from app.tasks import publish_tasks
 from .. import main
 from ... import db
 from ...models import (
     User,
     Brief,
+<<<<<<< HEAD
     TeamBrief,
     Team
 )
@@ -26,3 +42,29 @@ def get_team(team_id):
 @main.route('/admin/buyers/<int:brief_id>/teams', methods=['GET'])
 def get_teams_by_brief_id(brief_id):
     return jsonify(team_business.get_teams_by_brief_id(brief_id))
+=======
+    TeamBrief, 
+    Team
+)
+from ...utils import (
+    get_json_from_request, get_int_or_400, json_has_required_keys, pagination_links,
+    get_valid_page_or_1, get_request_page_questions, validate_and_return_updater_request,
+    get_positive_int_or_400
+)
+from ...service_utils import validate_and_return_lot, filter_services
+
+from ...datetime_utils import parse_time_of_day, combine_date_and_time
+
+@main.route('/team/<int:team_id>', methods=['GET'])
+def get_team(team_id):
+    print(team_id)
+    team = (
+        Team
+        .query
+        .filter(
+            Team.id == team_id
+        )
+        .first_or_404()
+    )
+    return jsonify(teamsId=team.id)
+>>>>>>> working Team_id added teams in _init_.py
