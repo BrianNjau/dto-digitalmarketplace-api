cff489c39f1b8f713fedebdef4c14855c5d4e945
Reshma Abraham <reshmaabraham@DTAs-MacBook-Pro.local>
2019-09-26 11:57:39 +1000

    fixing styling issues

--- app/main/views/teams.py
+++ app/main/views/teams.py
@@ -15,14 +15,7 @@ from ...models import (
     Team
 )
 
-
 @main.route('/admin/team/<int:team_id>', methods=['GET'])
 def get_team(team_id):
     team = team_business.get_team(team_id, True)
-    briefs = team_business.get_team_briefs(team_id)
-    return jsonify(team=team, briefs=briefs)
-
-
-@main.route('/admin/buyers/<int:brief_id>/teams', methods=['GET'])
-def get_teams_by_brief_id(brief_id):
-    return jsonify(team_business.get_teams_by_brief_id(brief_id))
+    return jsonify(team)
