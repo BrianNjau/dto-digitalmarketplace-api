e3cd13687648a18930a26a950793ba9ccaa474bd
Reshma Abraham <reshmamabraham@gmail.com>
2019-10-08 09:54:45 +1100

    removed unused imports

--- app/main/views/teams.py
+++ app/main/views/teams.py
@@ -14,15 +14,6 @@ from ...models import (
     TeamBrief,
     Team
 )
-from ...utils import (
-    get_json_from_request, get_int_or_400, json_has_required_keys, pagination_links,
-    get_valid_page_or_1, get_request_page_questions, validate_and_return_updater_request,
-    get_positive_int_or_400
-)
-from ...service_utils import validate_and_return_lot, filter_services
-
-from ...datetime_utils import parse_time_of_day, combine_date_and_time
-
 
 @main.route('/admin/team/<int:team_id>', methods=['GET'])
 def get_team(team_id):
