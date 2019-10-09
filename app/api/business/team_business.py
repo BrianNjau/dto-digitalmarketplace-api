ac6df7cfed414f37cc037cc4d14b0c6d2d4b3222
Reshma Abraham <reshmamabraham@gmail.com>
2019-10-10 10:06:39 +1100

    changing return

--- app/api/business/team_business.py
+++ app/api/business/team_business.py
@@ -305,4 +305,4 @@ def get_team_briefs(team_id):
 
 
 def get_teams_by_brief_id(brief_id):
-    return team_service.is_brief_id_in_teams(brief_id)
+    return team_service.get_teams_by_brief_id(brief_id)
