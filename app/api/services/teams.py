c083353f2b77ee53df06288c29575e1bfb6b7456
Reshma Abraham <reshmamabraham@gmail.com>
2019-10-11 15:06:07 +1100

    fixed styling

--- app/api/services/teams.py
+++ app/api/services/teams.py
@@ -218,6 +218,7 @@ class TeamService(Service):
             .filter(User.agency_id == agency_id)
             .subquery()
         )
+        
         results = (
             db
             .session
