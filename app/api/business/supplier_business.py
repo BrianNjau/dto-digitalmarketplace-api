fb46cd42d332b3d82a1d3115f6c8bbd30f7b6c4f
Reshma Abraham <reshmamabraham@gmail.com>
2020-02-12 13:12:30 +1100

    this prints the body of returned data

--- app/api/business/supplier_business.py
+++ app/api/business/supplier_business.py
@@ -7,6 +7,8 @@ from app.api.business.agreement_business import (get_current_agreement,
                                                  has_signed_current_agreement)
 from app.api.business.validators import SupplierValidator
 from app.api.services import application_service, key_values_service, suppliers
+import requests as req
+import os, ssl
 
 
 def abn_is_used(abn):
@@ -19,6 +21,28 @@ def abn_is_used(abn):
         return True
     return False
 
+def validate_abn(abn):
+        # GUID
+    authenticationGuid = '7ef41140-8406-40b4-8bf2-12582b5404ce'
+    includeHistoricalDetails = 'N'
+    abn = abn
+    # if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    #     getattr(ssl, '_create_unverified_context', None)):
+    #     ssl._create_default_https_context = ssl._create_unverified_contexta
+
+    includeHistoricalDetails = 'N'
+
+
+    # need to import os, ssl since i get this error URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:727)>
+    conn = req.get('https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/SearchByABNv201205?searchString=' + abn + '&includeHistoricalDetails='+ includeHistoricalDetails +'&authenticationGuid='+ authenticationGuid)
+    # conn.status_code == requests.codes.ok will print True or if you do conn.status_code will print out 200
+    print('HELLOOOOOOOOO')
+    print(conn.text)
+    # returnedXML = conn.get_data()
+    # f = open('output.xml', 'wb')
+    # f.write(returnedXML)
+    # f.close
+    # print(returnedXML)
 
 def get_supplier_messages(code, skip_application_check):
     applications = application_service.find(
