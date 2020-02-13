05bde5fc33259cc0ad92dea75cec61f38e62eb58
Reshma Abraham <reshmamabraham@gmail.com>
2020-02-13 15:16:03 +1100

    cleaning the code

--- app/api/business/supplier_business.py
+++ app/api/business/supplier_business.py
@@ -8,17 +8,7 @@ from app.api.business.agreement_business import (get_current_agreement,
 from app.api.business.validators import SupplierValidator
 from app.api.services import application_service, key_values_service, suppliers
 import requests as req
-import xml.etree.ElementTree as ET
-# from xml.etree.ElementTree import parse
-# from lxml import etree
-from lxml.etree import fromstring
-# from lxml import cssselect
-# import cssselect2
-# import xmltodict
 import re
-
-from lxml import etree, html
-
 
 def abn_is_used(abn):
     abn = "".join(abn.split())
@@ -36,68 +26,12 @@ def validate_abn(abn):
     includeHistoricalDetails = 'N'
     abn = abn
     includeHistoricalDetails = 'N'
-
-    # need to import os, ssl since i get this error URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:727)>
     conn = req.get('https://abr.business.gov.au/abrxmlsearch/AbrXmlSearch.asmx/SearchByABNv201205?searchString=' + abn + '&includeHistoricalDetails='+ includeHistoricalDetails +'&authenticationGuid='+ authenticationGuid)
     # conn.status_code == requests.codes.ok will print True or if you do conn.status_code will print out 200
-
     a = conn.content
-    print(a)
-    # b = etree.fromstring(a)
-
-    regex = r'<organisationName>(.*)</organisationName>'
-    x = re.search("<organisationName>(.*?)</organisationName>", a)
     c = re.findall(r'<organisationName>(.*?)</organisationName>', a)
-    # print x.group()
+    # Just finding the first one
     print(c[0])
-
-    
-    # print(a)
-    # b = etree.fromstring(a)
-    
-
-    # req = requests.get("http://www.nbp.pl/kursy/xml/LastC.xml", stream=True)
-    # req.raw.decode_content = True  # ensure transfer encoding is honoured
-    # b = etree.parse(req.raw)
-
-    # xml_doc= parse(conn)
-    # for item in xmldoc.iterfind('')
-
-    # data.xmltodict.parse(data)
-    
-
-    
-    # print('Organisation_Name:', tree.find('organisationName'))
-    # root = conn.getroot()
-    # for x in root:
-    #     print x.find("organisationName").text
-    # print("this is the whole XML output")
-    # print(conn.content)
-    # returnXML = conn.content
-    # root = ET.fromstring(returnXML)
-
-    # print("PLEASE WORK")
-    # result = root.find("organisationName").text
-
-    # result = root.findall("./mainName[organisationName]")
-    # print(result)
-
-    #a = xml.find('{http://abr.business.gov.au/ABRXMLSearch/}).attrib['organisationName']
-    # a = returnXML.getElementsByTagName("organisationName")[0]
-    # print(a)
-    
-    # print(PLEASE WORK)
-    # a = getElementsByTagName('organusationName')
-    # print(a)
-
-    # for child in root.iter('*'):
-    #     print(child.tag)
-
-
-    # print('ATTEMPTINGT TO PRINT BUSINESS NAME')
-    # for child in root.iter('{http://abr.business.gov.au/ABRXMLSearch/}organisationName'):
-    #     print(child.tag, child.attrib)
-
 
 def get_supplier_messages(code, skip_application_check):
     applications = application_service.find(
