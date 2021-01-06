c78b153c3b173eeed9fd94d0fc85f3940e9b1d1e
Reshma-M-A <reshmamabraham@gmail.com>
2021-01-07 05:04:59 +1100

    wip

--- app/api/services/case_study.py
+++ app/api/services/case_study.py
@@ -12,50 +12,34 @@ class CaseStudyService(Service):
         super(CaseStudyService, self).__init__(*args, **kwargs)
 
     def get_approved_case_studies_by_supplier_code(self, supplier_code, domain_id):
-        # check if the case study id exists
-        exists = (
+        subquery = (
             db
             .session
-            .query(CaseStudy.id)
-            .filter(
-                CaseStudy.supplier_code == supplier_code,
-                CaseStudy.status == 'approved',
-                Domain.id == domain_id)
-            .scalar()
-        ) is not None
-
-        if exists:
-            subquery = (
-                db
-                .session
-                .query(
-                    CaseStudy.id.label('cs_id'),
-                    CaseStudy.data.label('case_study_data'),
-                    Domain.name.label('category_name')
-                )
-                .join(Domain, Domain.name == CaseStudy.data['service'].astext)
-                .filter(CaseStudy.supplier_code == supplier_code,
-                        CaseStudy.status == 'approved',
-                        Domain.id == domain_id)
-                .subquery()
+            .query(
+                CaseStudy.id.label('cs_id'),
+                CaseStudy.data.label('case_study_data'),
+                Domain.name.label('category_name')
             )
+            .join(Domain, Domain.name == CaseStudy.data['service'].astext)
+            .filter(CaseStudy.supplier_code == supplier_code,
+                    CaseStudy.status == 'approved',
+                    Domain.id == domain_id)
+            .subquery()
+        )
 
-            result = (
-                db
-                .session
-                .query(
-                    subquery.c.category_name,
-                    func.json_agg(
-                        func.json_build_object(
-                            'id', subquery.c.cs_id,
-                            'data', subquery.c.case_study_data
-                        )
-                    ).label('cs_data')
-                )
-                .group_by(subquery.c.category_name)
+        result = (
+            db
+            .session
+            .query(
+                subquery.c.category_name,
+                func.json_agg(
+                    func.json_build_object(
+                        'id', subquery.c.cs_id,
+                        'data', subquery.c.case_study_data
+                    )
+                ).label('cs_data')
             )
-            results = result.one_or_none()._asdict()
-            return results if results else {}
-
-        else:
-            return {}
+            .group_by(subquery.c.category_name)
+        )
+        results = result.one_or_none()
+        return results._asdict() if results else {}
